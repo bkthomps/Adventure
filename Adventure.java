80dcd73f33d1eda5335c13a0774bf805f18f2ee6
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-07 21:33:48 -0500

    Refactor

--- Adventure.java
+++ Adventure.java
@@ -1,13 +1,13 @@
-/**
+/*
  * Bailey Thompson
- * Adventure (1.3.4)
- * 8 January 2017
- * Downloads: Requires jaco mp3 player
+ * Adventure (1.4.0)
+ * 7 February 2017
+ * Dependencies: jaco mp3 player
  * Info: RPG Game! Made during a hackathon.
  */
-package adventure;
 
 import jaco.mp3.player.MP3Player;
+
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -20,7 +20,8 @@ import javax.swing.JPanel;
 
 public class Adventure {
 
-    private static final ImageIcon ICONADVENTURE = new ImageIcon("Adventure.png");
+    private static final ImageIcon ICON_ADVENTURE = new ImageIcon("Adventure.png");
+    private static final String SOUND_FILE = "Adventure.mp3";
     private final JFrame frame = new JFrame("Adventure");
     private final JLabel text1 = new JLabel("Welcome To Adventure! This Game Was Created By Bailey Thompson");
     private final JLabel text2 = new JLabel("To Start The Game, Please Pick A Direction To Move In!");
@@ -34,7 +35,7 @@ public class Adventure {
     private final JButton run = new JButton("Run/Leave");
     private final JPanel textPanel = new JPanel(), infoAPanel = new JPanel(), infoBPanel = new JPanel();
     private final JPanel panelMid = new JPanel(), panelBot = new JPanel(), panelNeg = new JPanel();
-    private int xp, level = 1, currentHP = 20, weapon, item, lastMove, randomNum, encounter, half;
+    private int xp, level = 1, currentHP = 20, weapon, item, randomNum, encounter;
     private boolean newRound = true;
 
     public static void main(String[] args) {
@@ -43,18 +44,19 @@ public class Adventure {
     }
 
     private void prepareGUI() {
-        //playing the sound file on repeat
-        MP3Player player = new MP3Player();
-        player.addToPlayList(new File("Adventure.mp3"));
-        player.setRepeat(true);
-        player.play();
+        final File SOUND_HANDLE = new File(SOUND_FILE);
+        if (SOUND_HANDLE.exists()) {
+            MP3Player player = new MP3Player();
+            player.addToPlayList(SOUND_HANDLE);
+            player.setRepeat(true);
+            player.play();
+        }
 
-        //creating GUI frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(400, 400);
         frame.setLocationRelativeTo(null);
-        frame.setIconImage(ICONADVENTURE.getImage());
+        frame.setIconImage(ICON_ADVENTURE.getImage());
         frame.setVisible(true);
         frame.getContentPane().setBackground(Color.gray);
 
@@ -129,7 +131,7 @@ public class Adventure {
 
         btnWeapon.addActionListener((ActionEvent e) -> {
             if (!newRound) {
-                randomNum = ((int) (Math.random() * ((100 - 0) + 1))) + 0;
+                randomNum = generateNumber(100);
                 if (randomNum > (100 - (2 * weapon + 30))) {
                     //if user wins random test
                     newRound = true;
@@ -158,7 +160,7 @@ public class Adventure {
 
         run.addActionListener((ActionEvent e) -> {
             if (!newRound) {
-                randomNum = ((int) (Math.random() * ((100 - 0) + 1))) + 0;
+                randomNum = generateNumber(100);
                 if (randomNum > 50) {
                     //if user wins random test
                     newRound = true;
@@ -213,7 +215,7 @@ public class Adventure {
                 } else if (encounter == 5 || encounter == 6) {
                     text2.setText("It had no effect. Choose another action.");
                 } else {
-                    int bombDamage = ((int) (Math.random() * ((item - 0) + 1))) + 0;
+                    int bombDamage = generateNumber(item);
                     if (bombDamage > 2) {
                         text2.setText("You killed it! What direction do you choose now?");
                         newRound = true;
@@ -247,10 +249,10 @@ public class Adventure {
         south.setEnabled(false);
         west.setEnabled(false);
         east.setEnabled(false);
-        encounter = ((int) (Math.random() * ((11 - 0) + 1))) + 0;
+        encounter = generateNumber(11);
         while ((item != 0 && (encounter == 8 || encounter == 10))
                 || (weapon != 0 && (encounter == 9 || encounter == 11))) {
-            encounter = ((int) (Math.random() * ((11 - 0) + 1))) + 0;
+            encounter = generateNumber(11);
         }
         if (encounter == 10 || encounter == 11) {
             encounter -= 2;
@@ -281,108 +283,50 @@ public class Adventure {
                 text1.setText("A dying girl blocks your path. What do you do?");
                 break;
             case 8:
-                half = ((int) (Math.random() * ((2 - 0) + 1))) + 0;
+                int half = generateNumber(2);
                 if (half == 0) {
                     text1.setText("An elf gave you an item. What do you do?");
                 } else {
                     text1.setText("A sand person gave you an item. What do you do?");
                 }
-                item = ((int) (Math.random() * ((9 - 1) + 1))) + 1;
+                item = generateNumber(9) + 1;
                 break;
             case 9:
-                half = ((int) (Math.random() * ((2 - 0) + 1))) + 0;
+                half = generateNumber(2);
                 if (half == 0) {
                     text1.setText("An old man gave you a weapon. What do you do?");
                 } else {
                     text1.setText("A sentient robot gave you a weapon. What do you do?");
                 }
-                weapon = ((int) (Math.random() * ((9 - 1) + 1))) + 1;
+                weapon = generateNumber(9) + 1;
                 break;
         }
     }
 
     private void updateGUI() {
         if (currentHP > 0) {
-            switch (weapon) {
-                case 0:
-                    infoB1.setText("  Weapon: Fists");
-                    break;
-                case 1:
-                    infoB1.setText("  Weapon: Stick");
-                    break;
-                case 2:
-                    infoB1.setText("  Weapon: Rock");
-                    break;
-                case 3:
-                    infoB1.setText("  Weapon: Dagger");
-                    break;
-                case 4:
-                    infoB1.setText("  Weapon: S. Sword");
-                    break;
-                case 5:
-                    infoB1.setText("  Weapon: L. Sword");
-                    break;
-                case 6:
-                    infoB1.setText("  Weapon: L. Bow");
-                    break;
-                case 7:
-                    infoB1.setText("  Weapon: R. Bow");
-                    break;
-                case 8:
-                    infoB1.setText("  Weapon: C. Bow");
-                    break;
-                case 9:
-                    infoB1.setText("  Weapon: Crossbow");
-                    break;
-            }
-            switch (item) {
-                case 0:
-                    infoB2.setText("  Item: Nothing");
-                    break;
-                case 1:
-                    infoB2.setText("  Item: Petty Potion");
-                    break;
-                case 2:
-                    infoB2.setText("  Item: Small Potion");
-                    break;
-                case 3:
-                    infoB2.setText("  Item: Average Potion");
-                    break;
-                case 4:
-                    infoB2.setText("  Item: Effective Potion");
-                    break;
-                case 5:
-                    infoB2.setText("  Item: Campfire");
-                    break;
-                case 6:
-                    infoB2.setText("  Item: Petty Bomb");
-                    break;
-                case 7:
-                    infoB2.setText("  Item: Small Bomb");
-                    break;
-                case 8:
-                    infoB2.setText("  Item: Average Bomb");
-                    break;
-                case 9:
-                    infoB2.setText("  Item: Effective Bomb");
-                    break;
-            }
-            //setting the level
+            final String[] WEAPONS = {"Fists", "Stick", "Rock", "Dagger", "S. Sword", "L. Sword",
+                    "L. Bow", "R. Bow", "C. Bow", "Crossbow"};
+            infoB1.setText("  Weapon: " + WEAPONS[weapon]);
+
+            final String[] ITEMS = {"Nothing", "Petty Potion", "Small Potion", "Average Potion", "Effective Potion",
+                    "Campfire", "Petty Bomb", "Small Bomb", "Average Bomb", "Effective Bomb"};
+            infoB2.setText("  Item: " + ITEMS[item]);
+
             int previousLevel = level;
             level = (int) (Math.floor((xp / 5)) + 1);
-            //resetting hp
+
             if (previousLevel != level) {
                 currentHP = 20;
             }
-            //updates what level the user is displayed as having
+
             infoA1.setText("  Health: " + currentHP);
             infoA2.setText("  Level: " + level);
         } else {
-            //displaying that the user has died
             text1.setText("You died. Restart the application to play again! :(");
             text2.setText("On your adventure, you made it to Level " + level);
             infoA1.setText("  Health: 0");
-            //disabling all buttons
+
             btnWeapon.setEnabled(false);
             run.setEnabled(false);
             btnItem.setEnabled(false);
@@ -391,5 +335,9 @@ public class Adventure {
             west.setEnabled(false);
             east.setEnabled(false);
         }
+    }
+
+    private int generateNumber(int max) {
+        return (int) (Math.random() * (max + 1));
     }
 }
